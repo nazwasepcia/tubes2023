6c2a35580ce0afa2c256be7c0aa58f0ec378fba4
tri wahyu nugroho <triwahyunugroho_19@yahoo.com>
2023-12-14 20:44:47 +0700

    fix db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -7,7 +7,7 @@ $password = "";
 try{
     $pdo = new PDO("mysql:host=$host;dbname=$dbname",$user,$password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected succesfully";
+    // echo "Connected succesfully";
 } catch(PDOException $e) {
     echo "Connected failed: " . $e->getMessage();
 }
