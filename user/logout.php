99f71631ef173dd6f2d46e2d6edc71ba47b0f5b2
tri wahyu nugroho <triwahyunugroho_19@yahoo.com>
2023-12-14 20:40:41 +0700

    logout.php

---
+++ user/logout.php
@@ -0,0 +1,14 @@
+<?php
+// Mulai session
+session_start();
+
+// Menghapus semua variabel session
+$_SESSION = array();
+
+// Menghancurkan session
+session_destroy();
+
+// Mengarahkan ke halaman login (atau halaman lain yang Anda inginkan)
+header("Location: login.php");
+exit;
+?>
